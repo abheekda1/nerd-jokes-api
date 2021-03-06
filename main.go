c50426ad52f77f6e309a2e6e2c96366f395cef5e
Abheek Dhawan <abheekd@protonmail.com>
2021-03-06 12:26:18 -0600

    "Reformat code to use router"
    This allows for much more concise code with variables instead of
    hard-coding the endpoint for each different subject. The format for the
    JSON file was also modified to accommodate for this change. The HTML
    file was also slightly changed, but it will be changed even more soon as
    it does not have buttons for all the endpoints.

--- main.go
+++ main.go
@@ -8,13 +8,15 @@ import (
 	"math/rand"
 	"net/http"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 func Marshal(input interface{}) ([]byte, error) {
 	return json.Marshal(input)
 }
 
-func generalJokes(w http.ResponseWriter, r *http.Request) {
+/*func generalJokes(w http.ResponseWriter, r *http.Request) {
 	jokesJSON, err := ioutil.ReadFile("static/jokes.json")
 	if err != nil {
 		fmt.Println(err)
@@ -112,13 +114,114 @@ func scienceJokes(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Println("Endpoint Hit: Science Jokes Page")
+}*/
+
+func allJokesBySubject(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["subject"]
+
+	jokesJSON, err := ioutil.ReadFile("static/jokes.json")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	type Joke struct {
+		Subject    string
+		isOneliner bool
+		Title      string
+		Oneliner   string
+		Setup      string
+		Punchline  string
+	}
+
+	var joke []Joke
+	json.Unmarshal([]byte(jokesJSON), &joke)
+
+	for _, subJoke := range joke {
+		if subJoke.Subject == key {
+			fmt.Fprintf(w, "\"Subject\": \"%v\", \"Title\": \"%v\", \"Oneliner\": \"%v\", \"Setup\": \"%v\", \"Punchline\": \"%v\"", subJoke.Subject, subJoke.Title, subJoke.Oneliner, subJoke.Setup, subJoke.Punchline)
+		}
+	}
+}
+
+func randomJokeBySubject(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["subject"]
+
+	jokesJSON, err := ioutil.ReadFile("static/jokes.json")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	type Joke struct {
+		Subject    string
+		isOneliner bool
+		Title      string
+		Oneliner   string
+		Setup      string
+		Punchline  string
+	}
+
+	var joke []Joke
+	json.Unmarshal([]byte(jokesJSON), &joke)
+
+	var jokesBySubject []string
+
+	for _, subJoke := range joke {
+		if subJoke.Subject == key {
+			jokesBySubject = append(jokesBySubject, fmt.Sprintf("\"Subject\": \"%v\", \"Title\": \"%v\", \"Oneliner\": \"%v\", \"Setup\": \"%v\", \"Punchline\": \"%v\"", subJoke.Subject, subJoke.Title, subJoke.Oneliner, subJoke.Setup, subJoke.Punchline))
+		}
+	}
+
+	fmt.Fprintf(w, jokesBySubject[rand.Intn(len(jokesBySubject))])
+
+}
+
+func allJokes(w http.ResponseWriter, r *http.Request) {
+	jokesJSON, err := ioutil.ReadFile("static/jokes.json")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Fprintf(w, string(jokesJSON))
+}
+
+func randomJoke(w http.ResponseWriter, r *http.Request) {
+	jokesJSON, err := ioutil.ReadFile("static/jokes.json")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	type Joke struct {
+		Subject    string
+		isOneliner bool
+		Title      string
+		Oneliner   string
+		Setup      string
+		Punchline  string
+	}
+
+	var joke []Joke
+	json.Unmarshal([]byte(jokesJSON), &joke)
+
+	index := rand.Intn(len(joke))
+	randomJoke, _ := Marshal(joke[index])
+	fmt.Fprintf(w, string(randomJoke))
+
+	fmt.Println("Endpoint Hit: Science Jokes Page")
 }
 
 func handleRequests() {
-	http.Handle("/", http.FileServer(http.Dir("./static")))
-	http.HandleFunc("/jokes/random", generalJokes)
-	http.HandleFunc("/jokes/science/random", scienceJokes)
-	log.Fatal(http.ListenAndServe(":3587", nil))
+	router := mux.NewRouter().StrictSlash(true)
+	router.Handle("/", http.FileServer(http.Dir("./static")))
+	router.HandleFunc("/jokes", allJokes)
+	router.HandleFunc("/jokes/random", randomJoke)
+	router.HandleFunc("/jokes/{subject}", allJokesBySubject)
+	router.HandleFunc("/jokes/random/{subject}", randomJokeBySubject)
+	log.Fatal(http.ListenAndServe(":3587", router))
 }
 
 func main() {
